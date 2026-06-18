debf52fc46a2a380b4d0a830ce5467b91960af7b
David Josh Carnaje <david.carnaje@cspb.edu.ph>
2026-06-18 18:03:28 +0800

    feat: implement Electron main process with print workflow and update project configuration

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,4 +1,6 @@
 export const isNativeApp = typeof window !== 'undefined' && (
   window.navigator.userAgent.toLowerCase().includes('electron') ||
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   (window as any).process?.versions?.electron !== undefined
 );
+
