4d77b17073db7a1ec8822a24ddc4e57df547a8d9
David Josh Carnaje <david.carnaje@cspb.edu.ph>
2026-06-18 18:12:22 +0800

    feat: implement PDF printing service and configure application window and icon metadata

--- electron/main.ts
+++ electron/main.ts
@@ -74,7 +74,7 @@ let win: BrowserWindow | null
 function createWindow() {
   win = new BrowserWindow({
     title: "PrintFlow",
-    icon: path.join(process.env.VITE_PUBLIC, 'electron-vite.svg'),
+    icon: path.join(process.env.VITE_PUBLIC, 'printflow-logo.png'),
     webPreferences: {
       preload: path.join(__dirname, 'preload.mjs'),
     },
